63adff2e1d8954e7d0d8f5de53dff52fc15f0d5c
Sergey <perfer79@gmail.com>
2022-05-23 11:58:51 +0700

    task19 Добавлен второй вариант палиндрома

--- task19_Palindrom/Program.cs
+++ task19_Palindrom/Program.cs
@@ -4,21 +4,33 @@
 // 12821 -> да
 
 //Первый вариант
-Console.WriteLine("Введите 5-значное число ");
-int number = Convert.ToInt32(Console.ReadLine());
-int Number1 = (number / 10000);
-int Number5 = (number % 10);
-//Console.WriteLine(Number1+" "+Number5);
-int Number2 = (number / 1000);
-int Number4 = (number % 100);
-//Console.WriteLine(Number2+" "+Number4);
-if(Number1 == Number5 || Number2 == Number4)
-{
-    Console.WriteLine($"{number} -> да");
-}
-else
-{
-    Console.WriteLine($"{number} -> нет");
-}
+// Console.WriteLine("Введите 5-значное число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int Number1 = (number / 10000);
+// int Number5 = (number % 10);
+// //Console.WriteLine(Number1+" "+Number5); на память
+// int Number2 = (number / 1000);
+// int Number4 = (number % 100);
+// //Console.WriteLine(Number2+" "+Number4); на память
+// if(Number1 == Number5 || Number2 == Number4)
+// {
+//     Console.WriteLine($"{number} -> да");
+// }
+// else
+// {
+//     Console.WriteLine($"{number} -> нет");
+// }
 
+// Второй вариант
+Console.WriteLine("Введите палиндром ");
+string number = Console.ReadLine();
+bool Palin = true;
+
+for (int i = 0; i < (number.Length - 1) / 2; i++)
+    if (number[i] != number[number.Length - i - 1])
+        Palin = false;
+
+if (Palin)
+    Console.WriteLine("Палиндром");
+else Console.WriteLine("Не палиндром");
 
